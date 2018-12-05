2a1be7ae3c06c2d079380b4b9944a6b239465d51
Fan Feng <fanfengng@yahoo.com>
2018-12-05 22:02:07 +0900

    Added contents in sideBar

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
 let root_url = "http://comp426.cs.unc.edu:3001/";
 
+let city='';
+let trip_type="round trip";
+
 $(document).ready(() => {
   $("#login").on("click", () => {
     let data = {
@@ -21,15 +24,15 @@ $(document).ready(() => {
         if (jqXHR.status === 0) {
           $("#status").html(
             '<strong class="text-danger">Unable to reach server. Make sure you are online. If you are off-campus, make sure you are connected to the VPN.</strong>'
-          );
+            );
         } else if (jqXHR.status === 401) {
           $("#status").html(
             '<strong class="text-danger">Incorrect username and/or password.</strong>'
-          );
+            );
         } else {
           $("#status").html(
             '<strong class="text-danger">An unknown error occurred.</strong>'
-          );
+            );
         }
       }
     });
@@ -54,6 +57,22 @@ $(document).ready(() => {
   $("#flight").on("click", () => {
     loadFlight();
   });
+
+  $('input[type=radio][name=flight]').change(function() {
+    if (this.value == 'round-trip') {
+      $('#flight').html('Flight: Departure');
+      $('#return-date').show();
+    }
+    else if (this.value == 'one-way') {
+      $('#flight').html('Flight');
+      $('#return-date').hide();
+    }
+  });
+
+  $("#search").on("click", () => {
+    showFlight();
+  });
+
 });
 
 function loadHome() {
@@ -75,25 +94,48 @@ function loadCity(cityName) {
   $("#city-page").show();
   $("#city-header-title").text(cityName);
   if (cityName == "San Francisco") {
+    city='San Francisco';
     $(".city").css("background-image", "url(pic/sfo.jpg)");
   } else if (cityName == "New York") {
+    city='New York';
     $(".city").css("background-image", "url(pic/nyc.jpg)");
   } else if (cityName == "Chicago") {
+    city='Chicago';
     $(".city").css("background-image", "url(pic/chi.jpeg)");
   } else if (cityName == "Los Angeles") {
+    city='Los Angeles';
     $(".city").css("background-image", "url(pic/la.jpg)");
   }
+
   loadRestaurant();
 }
 
 function loadRestaurant() {
   $("#rest").css("background-color", "#c8255b");
   $("#flight").css("background-color", "#86193d");
-  $("#result").text("Display restaurant results here");
+  $("#result").html("");
 }
 
 function loadFlight() {
   $("#rest").css("background-color", "#86193d");
   $("#flight").css("background-color", "#c8255b");
-  $("#result").text("Display flight results here");
+  $("#result").html("");
+}
+
+function showFlight() {
+  loadFlight();
+
+  $.ajax(root_url + "/airlines", {
+    type: 'GET',
+    xhrFields: {
+      withCredentials: true
+    },
+    success: () => {
+      alert('success');
+    },
+    error: function(jqXHR, textStatus, errorThrown) {
+      $("#result").html("Uh oh, no flights found. Try departing from different city or choose a different date.");
+    }
+  });
+
 }
