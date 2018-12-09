1943e964f95397fbc75bba674b1bda8dab51fb72
Fan Feng <fanfengng@yahoo.com>
2018-12-09 11:28:59 +0900

    added auto complete

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 let root_url = "http://comp426.cs.unc.edu:3001/";
 let city='';
 let trip_type="round trip";
-let airport_names=[];
+let airport_cities=[];
 let airport_codes=[];
 let select1,select2=''; //selected to and return flights
 let trips = [];
@@ -65,19 +65,20 @@ $(document).ready(() => {
   });
 
   $('#create').on('click',(e)=>{
+    let trip='';
     let target=$(e.target);
     if(trip_type=='round trip'){
       let flight1=$($('#selected1').parents('.flight')[0]);
       let flight2=$($('#selected2').parents('.flight')[0]);
-      let trip= new RoundTrip(flight1.attr('airport1'),flight1.attr('airport2'),date1,date2,flight1.attr('number'),flight2.attr('number'),$(flight1.children('.date1')[0]).html(),$(flight1.children('.date2')[0]).html(),$(flight2.children('.date1')[0]).html(),$(flight2.children('.date2')[0]).html());
-
+      trip= new RoundTrip(flight1.attr('airport1'),flight1.attr('airport2'),date1,date2,flight1.attr('number'),flight2.attr('number'),$(flight1.children('.date1')[0]).html(),$(flight1.children('.date2')[0]).html(),$(flight2.children('.date1')[0]).html(),$(flight2.children('.date2')[0]).html());   
     }else{
       let flight=$($('#selected1').parents('.flight')[0]);
-      let trip= new SingleTrip(flight.attr('airport1'),flight.attr('airport2'),date1,flight.attr('number'),$(flight.children('.date1')[0]).html(),$(flight.children('.date2')[0]).html());
-      
+      trip= new SingleTrip(flight.attr('airport1'),flight.attr('airport2'),date1,flight.attr('number'),$(flight.children('.date1')[0]).html(),$(flight.children('.date2')[0]).html());  
     }
-    console.log(trip);
 
+    trips.push(trip);
+    console.log(trip);
+    console.log(trips);
     loadMyTrip();
   });
 
@@ -115,6 +116,7 @@ $(document).ready(() => {
     $('#buttom-b').children().each(function() {
       $( this ).hide();
     });
+    $('#flight2').html('');  
     findFlight(1);
   });
 
@@ -205,8 +207,8 @@ function loadCity(cityName) {
     city='Los Angeles';
     $(".city").css("background-image", "url(pic/la.jpg)");
   }
+  autocomplete(document.getElementById("depart_val"), airport_cities);
   loadRestaurant();
-
 }
 
 function loadRestaurant() {
@@ -349,7 +351,7 @@ function getFlight(id,handleData){
 }
 
 function getAirports(flight_id,departId,arrivalId){
-  $.ajax(root_url + "/airports?filter[depart_id]="+departId+"&filter[depart_id]", {
+  $.ajax(root_url + "/airports", {
     type: 'GET',
     xhrFields: {
       withCredentials: true
@@ -357,10 +359,10 @@ function getAirports(flight_id,departId,arrivalId){
     success: (response) => {
       if(response!=null || response!=''){
         for(let i=0; i<response.length; i++){
-          airport_names.push(response.name);
-          airport_codes.push(response.code);
+          airport_cities.push(response[i].city);
+          airport_codes.push(response[i].code);
         }
-
+        console.log(airport_cities);
       }else{
         alert("Can't get airport ID!");
       }
@@ -456,7 +458,6 @@ let RoundTrip = function(airport1, airport2, date1, date2, number1, number2, tim
 }
 
 //when one way trip
-
 let SingleTrip = function(airport1, airport2, date1, number1, time11,time12){
   this.airport1=airport1;
   this.airport2=airport2;
@@ -465,3 +466,101 @@ let SingleTrip = function(airport1, airport2, date1, number1, time11,time12){
   this.time12=time12;
 }
 
+
+//-------------------------------------------------------------------auto complete part
+function autocomplete(inp, arr) {
+  /*the autocomplete function takes two arguments,
+  the text field element and an array of possible autocompleted values:*/
+  var currentFocus;
+  /*execute a function when someone writes in the text field:*/
+  inp.addEventListener("input", function(e) {
+      var a, b, i, val = this.value;
+      /*close any already open lists of autocompleted values*/
+      closeAllLists();
+      if (!val) { return false;}
+      currentFocus = -1;
+      /*create a DIV element that will contain the items (values):*/
+      a = document.createElement("DIV");
+      a.setAttribute("id", this.id + "autocomplete-list");
+      a.setAttribute("class", "autocomplete-items");
+      /*append the DIV element as a child of the autocomplete container:*/
+      this.parentNode.appendChild(a);
+      /*for each item in the array...*/
+      for (i = 0; i < arr.length; i++) {
+        /*check if the item starts with the same letters as the text field value:*/
+        if (arr[i].substr(0, val.length).toUpperCase() == val.toUpperCase()) {
+          /*create a DIV element for each matching element:*/
+          b = document.createElement("DIV");
+          /*make the matching letters bold:*/
+          b.innerHTML = "<strong>" + arr[i].substr(0, val.length) + "</strong>";
+          b.innerHTML += arr[i].substr(val.length);
+          /*insert a input field that will hold the current array item's value:*/
+          b.innerHTML += "<input type='hidden' value='" + arr[i] + "'>";
+          /*execute a function when someone clicks on the item value (DIV element):*/
+              b.addEventListener("click", function(e) {
+              /*insert the value for the autocomplete text field:*/
+              inp.value = this.getElementsByTagName("input")[0].value;
+              /*close the list of autocompleted values,
+              (or any other open lists of autocompleted values:*/
+              closeAllLists();
+          });
+          a.appendChild(b);
+        }
+      }
+  });
+  /*execute a function presses a key on the keyboard:*/
+  inp.addEventListener("keydown", function(e) {
+      var x = document.getElementById(this.id + "autocomplete-list");
+      if (x) x = x.getElementsByTagName("div");
+      if (e.keyCode == 40) {
+        /*If the arrow DOWN key is pressed,
+        increase the currentFocus variable:*/
+        currentFocus++;
+        /*and and make the current item more visible:*/
+        addActive(x);
+      } else if (e.keyCode == 38) { //up
+        /*If the arrow UP key is pressed,
+        decrease the currentFocus variable:*/
+        currentFocus--;
+        /*and and make the current item more visible:*/
+        addActive(x);
+      } else if (e.keyCode == 13) {
+        /*If the ENTER key is pressed, prevent the form from being submitted,*/
+        e.preventDefault();
+        if (currentFocus > -1) {
+          /*and simulate a click on the "active" item:*/
+          if (x) x[currentFocus].click();
+        }
+      }
+  });
+  function addActive(x) {
+    /*a function to classify an item as "active":*/
+    if (!x) return false;
+    /*start by removing the "active" class on all items:*/
+    removeActive(x);
+    if (currentFocus >= x.length) currentFocus = 0;
+    if (currentFocus < 0) currentFocus = (x.length - 1);
+    /*add class "autocomplete-active":*/
+    x[currentFocus].classList.add("autocomplete-active");
+  }
+  function removeActive(x) {
+    /*a function to remove the "active" class from all autocomplete items:*/
+    for (var i = 0; i < x.length; i++) {
+      x[i].classList.remove("autocomplete-active");
+    }
+  }
+  function closeAllLists(elmnt) {
+    /*close all autocomplete lists in the document,
+    except the one passed as an argument:*/
+    var x = document.getElementsByClassName("autocomplete-items");
+    for (var i = 0; i < x.length; i++) {
+      if (elmnt != x[i] && elmnt != inp) {
+      x[i].parentNode.removeChild(x[i]);
+    }
+  }
+}
+/*execute a function when someone clicks in the document:*/
+document.addEventListener("click", function (e) {
+    closeAllLists(e.target);
+});
+}
