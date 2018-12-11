78791e6ffd74ce42a884c5479384fe583b7c7121
WanyiChen <wanyi.chen@swim.it>
2018-12-10 23:53:46 -0500

    change style

--- script.js
+++ script.js
@@ -1,17 +1,17 @@
 let root_url = "http://comp426.cs.unc.edu:3001/";
-let city='';
-let trip_type="round trip";
-let airport_names=[];
-let airport_codes=[];
-let select1,select2=''; //selected to and return flights
+let city = '';
+let trip_type = "round trip";
+let airport_names = [];
+let airport_codes = [];
+let select1, select2 = ''; //selected to and return flights
 let trips = [];
-let date1,date2='';
+let date1, date2 = '';
 
 /* Apply zomato API and impliment loadMyTip() and loadRestaurant() functions (12/10/2018 updated by jie)*/
-var zomato = require('zomato');
-var client = zomato.createClient({
-  userKey: '2ee136670cff3a1bd2e4a6d8427f1e36', //as obtained from [Zomato API](https://developers.zomato.com/apis)
-});
+//var zomato = require('zomato');
+//var client = zomato.createClient({
+//userKey: '2ee136670cff3a1bd2e4a6d8427f1e36', //as obtained from [Zomato API](https://developers.zomato.com/apis)
+//});
 
 $(document).ready(() => {
   getAirports();
@@ -36,15 +36,15 @@ $(document).ready(() => {
         if (jqXHR.status === 0) {
           $("#status").html(
             '<strong class="text-danger">Unable to reach server. Make sure you are online. If you are off-campus, make sure you are connected to the VPN.</strong>'
-            );
+          );
         } else if (jqXHR.status === 401) {
           $("#status").html(
             '<strong class="text-danger">Incorrect username and/or password.</strong>'
-            );
+          );
         } else {
           $("#status").html(
             '<strong class="text-danger">An unknown error occurred.</strong>'
-            );
+          );
         }
       }
     });
@@ -70,17 +70,17 @@ $(document).ready(() => {
     loadFlight();
   });
 
-  $('#create').on('click',(e)=>{
-    let target=$(e.target);
-    if(trip_type=='round trip'){
-      let flight1=$($('#selected1').parents('.flight')[0]);
-      let flight2=$($('#selected2').parents('.flight')[0]);
-      let trip= new RoundTrip(flight1.attr('airport1'),flight1.attr('airport2'),date1,date2,flight1.attr('number'),flight2.attr('number'),$(flight1.children('.date1')[0]).html(),$(flight1.children('.date2')[0]).html(),$(flight2.children('.date1')[0]).html(),$(flight2.children('.date2')[0]).html());
+  $('#create').on('click', (e) => {
+    let target = $(e.target);
+    if (trip_type == 'round trip') {
+      let flight1 = $($('#selected1').parents('.flight')[0]);
+      let flight2 = $($('#selected2').parents('.flight')[0]);
+      let trip = new RoundTrip(flight1.attr('airport1'), flight1.attr('airport2'), date1, date2, flight1.attr('number'), flight2.attr('number'), $(flight1.children('.date1')[0]).html(), $(flight1.children('.date2')[0]).html(), $(flight2.children('.date1')[0]).html(), $(flight2.children('.date2')[0]).html());
 
-    }else{
-      let flight=$($('#selected1').parents('.flight')[0]);
-      let trip= new SingleTrip(flight.attr('airport1'),flight.attr('airport2'),date1,flight.attr('number'),$(flight.children('.date1')[0]).html(),$(flight.children('.date2')[0]).html());
-      
+    } else {
+      let flight = $($('#selected1').parents('.flight')[0]);
+      let trip = new SingleTrip(flight.attr('airport1'), flight.attr('airport2'), date1, flight.attr('number'), $(flight.children('.date1')[0]).html(), $(flight.children('.date2')[0]).html());
+
     }
     // console.log(trip);
 
@@ -96,22 +96,21 @@ $(document).ready(() => {
     if (this.value == 'round-trip') {
       $('#flight').html('Flight: Departure');
       $('#return-date').show();
-      trip_type='round trip';
+      trip_type = 'round trip';
       $('#create').hide();
 
-      if($('#selected')){
+      if ($('#selected')) {
         $('#next-return').show();
       }
 
-    }
-    else if (this.value == 'one-way') {
+    } else if (this.value == 'one-way') {
       $('#flight').html('Flight');
       $('#return-date').hide();
-      trip_type='one way';
+      trip_type = 'one way';
       $('#flight1').show();
       $('#flight2').hide();
       $('#next-return').hide();
-      if($('#selected')){
+      if ($('#selected')) {
         $('#create').show();
       }
     }
@@ -119,30 +118,30 @@ $(document).ready(() => {
 
   $("#search").on("click", () => {
     $('#buttom-b').children().each(function() {
-      $( this ).hide();
+      $(this).hide();
     });
     findFlight(1);
   });
 
-  $('#next-return').on("click",()=>{
+  $('#next-return').on("click", () => {
     $('#next-return').hide();
     $('#prev').show();
     //make sure do not load again
-    if($('#flight2').children().length==0){
+    if ($('#flight2').children().length == 0) {
       findFlight(2);
-    }else{
+    } else {
       $('#flight1').hide();
       $('#flight2').show();
-      if($('#selected')){
+      if ($('#selected')) {
         $('#create').show();
       }
     }
 
     $('#flight').html('Flight: Arrival');
-    
+
   });
 
-  $('#prev').on('click',()=>{
+  $('#prev').on('click', () => {
     $('#prev').hide();
     $('#create').hide();
     $('#flight1').show();
@@ -151,24 +150,25 @@ $(document).ready(() => {
   });
 
 
-  $('body').on('click','button',(e)=>{
-    let target=$(e.target);
+  $('body').on('click', 'button', (e) => {
+    let target = $(e.target);
 
-    if(target.html()=='Select'){
-      if($('#flight1').css('display')=='block'){
-        select1=target;
+    if (target.html() == 'Select') {
+      if ($('#flight1').css('display') == 'block') {
+        select1 = target;
 
-        if (trip_type=='round trip') {
+        if (trip_type == 'round trip') {
           $('#next-return').show();
-        }else{
+        } else {
           $('#create').show();
         }
         $('#selected1').html('Select');
         $('#selected1').removeAttr('id');
         target.html('Selected');
         target.attr('id', 'selected1');
-      }else{
-        select2=target;
+        target.parent().css("background-color", "#9a93c2");
+      } else {
+        select2 = target;
         $('#create').show();
         $('#selected2').html('Select');
         $('#selected2').removeAttr('id');
@@ -189,7 +189,7 @@ function loadHome() {
 }
 
 /* loadMyTrip(): when clicked on MyTrip buttom (12/10/2018 updated by jie)
-	-- (1) show saved flights 
+	-- (1) show saved flights
 	-- (2) show saved restaurants  */
 function loadMyTrip() {
   $("#home-page").hide();
@@ -202,16 +202,16 @@ function loadCity(cityName) {
   $("#city-page").show();
   $("#city-header-title").text(cityName);
   if (cityName == "San Francisco") {
-    city='San Francisco';
+    city = 'San Francisco';
     $(".city").css("background-image", "url(pic/sfo.jpg)");
   } else if (cityName == "New York") {
-    city='New York';
+    city = 'New York';
     $(".city").css("background-image", "url(pic/nyc.jpg)");
   } else if (cityName == "Chicago") {
-    city='Chicago';
+    city = 'Chicago';
     $(".city").css("background-image", "url(pic/chi.jpeg)");
   } else if (cityName == "Los Angeles") {
-    city='Los Angeles';
+    city = 'Los Angeles';
     $(".city").css("background-image", "url(pic/la.jpg)");
   }
   loadRestaurant();
@@ -219,21 +219,21 @@ function loadCity(cityName) {
 }
 
 /* loadRestaurant(): when clicked on restaurant buttom (12/10/2018 updated by jie)
-	-- (1) list restaurants 
+	-- (1) list restaurants
 	-- (2) enable 'click to expand' and 'save to MyTrip' */
 function loadRestaurant() {
-  $("#rest").css("background-color", "#c8255b");
-  $("#flight").css("background-color", "#86193d");
+  $("#rest").css("background-color", "#9a93c2");
+  $("#flight").css("background-color", "#352b71");
   $('#flight-result').hide();
   $('#rest-result').show();
   $('#buttom-b').hide();
-  
-  
+
+
 }
 
 function loadFlight() {
-  $("#rest").css("background-color", "#86193d");
-  $("#flight").css("background-color", "#c8255b");
+  $("#rest").css("background-color", "#352b71");
+  $("#flight").css("background-color", "#9a93c2");
   $('#flight-result').show();
   $('#rest-result').hide();
   $('#buttom-b').show();
@@ -242,47 +242,47 @@ function loadFlight() {
 function findFlight(number) {
   loadFlight();
 
-  let date,airport,flight,logoURL,airlineName,flightDiv ='';
-  let arrivalId,departId = [];
-  let flights=[];
-  let flight_result_div='';
-  airport=$('#depart_val').val();
+  let date, airport, flight, logoURL, airlineName, flightDiv = '';
+  let arrivalId, departId = [];
+  let flights = [];
+  let flight_result_div = '';
+  airport = $('#depart_val').val();
 
-  if(airport==''){
+  if (airport == '') {
     alert('Please add airports');
     return;
   }
 
-  if(number==1){
-    flight_result_div=$('#flight1');
+  if (number == 1) {
+    flight_result_div = $('#flight1');
     $('#flight2').hide();
     flight_result_div.show();
 
-    date1=$('#date-1').val();
-    date=date1;
+    date1 = $('#date-1').val();
+    date = date1;
     //arrivalId=findAirportId(airport);
-    findAirportId(airport, function(output){
-      arrivalId=output;
+    findAirportId(airport, function(output) {
+      arrivalId = output;
     });
 
-    findAirportId(city, function(output){
-      departId=output;
+    findAirportId(city, function(output) {
+      departId = output;
     });
-  }else{
+  } else {
     $('#prev').show();
     $('#flight1').hide();
-    flight_result_div=$('#flight2');
+    flight_result_div = $('#flight2');
     flight_result_div.show();
 
-    date2=$('#date-2').val();
-    date=date2;
+    date2 = $('#date-2').val();
+    date = date2;
 
-    findAirportId(airport, function(output){
-      departId=output;
+    findAirportId(airport, function(output) {
+      departId = output;
     });
 
-    findAirportId(city, function(output){
-      arrivalId=output;
+    findAirportId(city, function(output) {
+      arrivalId = output;
     });
   }
 
@@ -290,7 +290,7 @@ function findFlight(number) {
   $('#loading').show();
 
   // Find instances on that day
-  $.ajax(root_url + "/instances?filter[date]="+date+"&filter[is_cancelled]=false", {
+  $.ajax(root_url + "/instances?filter[date]=" + date + "&filter[is_cancelled]=false", {
     type: 'GET',
     xhrFields: {
       withCredentials: true
@@ -298,34 +298,34 @@ function findFlight(number) {
     success: (response) => {
       flight_result_div.html('');
 
-      if(response!=null && response!='' && departId!=null && arrivalId!=null){
+      if (response != null && response != '' && departId != null && arrivalId != null) {
         //alert("here3 "+response.length+" "+departId.length+" "+arrivalId.length);
 
-        console.log(departId[0].id +" "+ arrivalId[0].id);
+        console.log(departId[0].id + " " + arrivalId[0].id);
 
-        for(let i=0; i<response.length; i++){
+        for (let i = 0; i < response.length; i++) {
 
-          getFlight(response[i].flight_id, function(output){
-            flight=output;
-            for(let j=0;j<departId.length;j++){
-              for(let k=0;k<arrivalId.length;k++){
-                if(flight.departure_id==departId[j].id && flight.arrival_id==arrivalId[k].id){
+          getFlight(response[i].flight_id, function(output) {
+            flight = output;
+            for (let j = 0; j < departId.length; j++) {
+              for (let k = 0; k < arrivalId.length; k++) {
+                if (flight.departure_id == departId[j].id && flight.arrival_id == arrivalId[k].id) {
                   console.log(flight);
-                  flightDiv=$('<div class="flight" id=f'+response[i].flight_id+' airport1='+departId[j].code+' airport2='+arrivalId[k].code+' number='+flight.number+'></div>');
-                  getAirlineInfo(flight.airline_id,flightDiv,function(output){
+                  flightDiv = $('<div class="flight" id=f' + response[i].flight_id + ' airport1=' + departId[j].code + ' airport2=' + arrivalId[k].code + ' number=' + flight.number + '></div>');
+                  getAirlineInfo(flight.airline_id, flightDiv, function(output) {
                     $('#loading').hide();
-                    flightDiv=output;
-                    flightDiv.append('<span class="date1">' +' '+flight.departs_at.substr(11,5)+'</span><span class="date2">'+' '+flight.arrives_at.substr(11,5)+'</span>');
+                    flightDiv = output;
+                    flightDiv.append('<span class="date1">' + ' ' + flight.departs_at.substr(11, 5) + '</span><span class="date2">' + ' ' + flight.arrives_at.substr(11, 5) + '</span>');
                     flightDiv.append('<button class="select">Select</button>');
                     flight_result_div.append(output);
                   });
                   flights.push(flight);
                 }
               }
-            }    
+            }
           });
         }
-      }else{
+      } else {
         $("#result").html("Uh oh, no instances found. Try departing from different city or choose a different date.");
       }
     },
@@ -337,9 +337,9 @@ function findFlight(number) {
 }
 
 
-function getFlight(id,handleData){
+function getFlight(id, handleData) {
 
-  let url=root_url + "/flights/"+id;
+  let url = root_url + "/flights/" + id;
 
   //console.log(url);
 
@@ -349,9 +349,9 @@ function getFlight(id,handleData){
       withCredentials: true
     },
     success: (response) => {
-      if(response!=null && response!=''){
+      if (response != null && response != '') {
         handleData(response);
-      }else{
+      } else {
         console.log("no suitable flight");
       }
     },
@@ -362,20 +362,20 @@ function getFlight(id,handleData){
 
 }
 
-function getAirports(flight_id,departId,arrivalId){
-  $.ajax(root_url + "/airports?filter[depart_id]="+departId+"&filter[depart_id]", {
+function getAirports(flight_id, departId, arrivalId) {
+  $.ajax(root_url + "/airports?filter[depart_id]=" + departId + "&filter[depart_id]", {
     type: 'GET',
     xhrFields: {
       withCredentials: true
     },
     success: (response) => {
-      if(response!=null || response!=''){
-        for(let i=0; i<response.length; i++){
+      if (response != null || response != '') {
+        for (let i = 0; i < response.length; i++) {
           airport_names.push(response.name);
           airport_codes.push(response.code);
         }
 
-      }else{
+      } else {
         alert("Can't get airport ID!");
       }
 
@@ -386,9 +386,9 @@ function getAirports(flight_id,departId,arrivalId){
   });
 }
 
-function findAirportId  (nameOrCode, handleData){
-  let airportId=[];
-  $.ajax(root_url + "/airports?filter[city]="+nameOrCode, {
+function findAirportId(nameOrCode, handleData) {
+  let airportId = [];
+  $.ajax(root_url + "/airports?filter[city]=" + nameOrCode, {
     type: 'GET',
     xhrFields: {
       withCredentials: true
@@ -396,10 +396,10 @@ function findAirportId  (nameOrCode, handleData){
     success: (response) => {
       //console.log(response);
 
-      if(response!=null || response!=''){
+      if (response != null || response != '') {
 
         handleData(response);
-      }else{
+      } else {
         alert("Can't get airort ID!");
       }
     },
@@ -424,19 +424,19 @@ function findAirportId  (nameOrCode, handleData){
   // });
 }
 
-function getAirlineInfo(id,flightDiv,handleData){
-  $.ajax(root_url + "/airlines/"+id, {
+function getAirlineInfo(id, flightDiv, handleData) {
+  $.ajax(root_url + "/airlines/" + id, {
     type: 'GET',
     xhrFields: {
       withCredentials: true
     },
     success: (response) => {
       console.log(response);
-      if(response!=''){
+      if (response != '') {
         response.name;
-        flightDiv.append($('<span class="airlineName">'+response.name+'   </span>'));
+        flightDiv.append($('<span class="airlineName">' + response.name + '   </span>'));
         handleData(flightDiv);
-      }else{
+      } else {
         flightDiv.html('No Airline Info Found');
         handleData(flightDiv);
       }
@@ -450,26 +450,25 @@ function getAirlineInfo(id,flightDiv,handleData){
 }
 
 //when round trip
-let RoundTrip = function(airport1, airport2, date1, date2, number1, number2, time11,time12,time21,time22){
-  this.airport1=airport1;
-  this.airport2=airport2;
-  this.date1=date1;
-  this.date2=date2;
-  this.number1=number1;
-  this.number2=number2;
-  this.time11=time11;
-  this.time12=time12;
-  this.time21=time21;
-  this.time22=time22;
+let RoundTrip = function(airport1, airport2, date1, date2, number1, number2, time11, time12, time21, time22) {
+  this.airport1 = airport1;
+  this.airport2 = airport2;
+  this.date1 = date1;
+  this.date2 = date2;
+  this.number1 = number1;
+  this.number2 = number2;
+  this.time11 = time11;
+  this.time12 = time12;
+  this.time21 = time21;
+  this.time22 = time22;
 }
 
 //when one way trip
 
-let SingleTrip = function(airport1, airport2, date1, number1, time11,time12){
-  this.airport1=airport1;
-  this.airport2=airport2;
-  this.date1=date1;
-  this.time11=time11;
-  this.time12=time12;
+let SingleTrip = function(airport1, airport2, date1, number1, time11, time12) {
+  this.airport1 = airport1;
+  this.airport2 = airport2;
+  this.date1 = date1;
+  this.time11 = time11;
+  this.time12 = time12;
 }
-
